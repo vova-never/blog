1a573c7e8f152bc7db815f4bbfe9475f3b816ce2
vova-never <127614781+vova-never@users.noreply.github.com>
2024-02-16 20:07:45 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask,url_for, redirect
+import os
+import setting as stg
+
+
+
+dirname = os.getcwd()
+
+app = Flask(__name__, static_folder=stg.path_static )
+app.config["secret_key = "] = stg.secret_key
+
+@app.route("/")
+def index():
+    url = url_for("templates/index.html")
+
+@app.route("/post/category")
+def post_category():
+    return "тут будуть категорії постів"
+
+@app.route("/post/view")
+def post_view():
+    return "тут буде пост"
+
+@app.route("/about")
+def about():
+    return "інфо про автора автора"
+
+
+
+app.run(debug = True)
---
+++ setting.py
@@ -0,0 +1,6 @@
+import os
+
+path = os.path.dirname(__file__) + os.sep
+path_static = path + "static" + os.sep
+
+secret_key = "66D3217D5E2E7A5BD53683D8C78AF"
