715c384fb720fb614d9d7b2168b5b7a21a7eb514
Vova <vova.volochaev.04@gmail.com>
2024-03-01 20:32:30 +0200

    lesson 1.03

--- db_scripts.py
+++ db_scripts.py
@@ -35,3 +35,32 @@ def getUser():
     user = cursor.fetchone()
     close()
     return user
+
+def getIdByCategory(category_name):
+    open()
+    cursor.execute(''' SELECT category_id 
+    FROM category 
+    WHERE category_name = (?) ''', [category_name])
+    category_id = cursor.fetchone()[0]
+    close()
+    return category_id
+
+
+def getPostsByCategory(category_name):
+    open()
+    cursor.execute(''' SELECT * FROM post, category
+        WHERE category_name = (?)
+        AND post.category_id = category.category_id
+        ORDER BY post_datetime DESC 
+        ''', [category_name])
+    posts = cursor.fetchall()
+    close()
+    return posts
+
+def addPost(category_id, post_text):
+    open()
+    cursor.execute('''INSERT INTO post (category_id, post_text)
+    VALUES (?), (?)
+    ''', [category_id, post_text])
+    connection.commit()
+    close()
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,url_for, redirect
+from flask import Flask, render_template, request,url_for, redirect
 import os
 import setting as stg
 import db_scripts as db
@@ -15,9 +15,16 @@ app.config["secret_key = "] = stg.secret_key
 def index():
     return render_template("index.html")
 
-@app.route("/post/category")
-def post_category():
-    return "тут будуть категорії постів"
+@app.route("/post/category/<category_name>", methods = ["GET", "POST"])
+def post_category(category_name):
+    category_id = db.getIdByCategory(category_name)
+
+    if request.method == "GET":
+        posts = db.getPostsByCategory(category_name)
+
+    return render_template("post_category.html", category_name = category_name,
+                           category_id = category_id,
+                           posts = posts)
 
 @app.route("/post/view")
 def post_view():
